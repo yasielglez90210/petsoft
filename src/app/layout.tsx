cfe126fbb4c67a3aef2817cdc6bac2e3f459973e
Yasiel <yasielglez90210@gmail.com>
2025-07-05 20:16:31 -0400

    base app

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Petsoft - Pet daycare software",
+  description: "Take care of people's pets responsively with Petsoft",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-sm text-zinc-900 bg-[#E5E8EC]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
